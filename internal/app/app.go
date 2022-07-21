670e5db56a01c35556a78b4f2583768bc544b5a8
古鸿尹 <guhongyin@top-list.com.cn>
2022-07-21 18:50:54 +0800

    update

--- internal/app/app.go
+++ internal/app/app.go
@@ -8,7 +8,7 @@ import (
 )
 
 type Order struct {
-	order_no string
+	order_no     string
 	pay_progress string
 }
 
@@ -16,21 +16,20 @@ func Run(cfg *config.Config) {
 	log.Println("项目开始运行")
 	//初始化mysql
 	dbObject := mysql.Init()
-	mysql, err := dbObject.New("default", cfg.Mysql)
+	db, err := dbObject.New("default", cfg.Mysql)
 	if err != nil {
 		log.Fatalf("db connect err:%s", err)
 	}
 
-
-	rows,err :=mysql.Query("select order_no,pay_progress from gops_order where order_no ='10104620210928151836273578'")
-	if err != nil{
+	rows, err := db.Query("select order_no,pay_progress from gops_order where order_no ='10104620210928151836273578'")
+	if err != nil {
 		log.Fatalf("db query err:%s", err)
 	}
 
-	for rows.Next(){	// 循环读取结果集中的数据
+	for rows.Next() { // 循环读取结果集中的数据
 		var u Order
-		err := rows.Scan(&u.order_no,&u.pay_progress)
-		if err != nil{
+		err := rows.Scan(&u.order_no, &u.pay_progress)
+		if err != nil {
 			log.Fatalf("db scan err:%s", err)
 		}
 		fmt.Println(u)
